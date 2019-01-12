3c8a790890789f0bbc5a1d8bc7e6bd2c17fc8a18
tltay <tayliting0108@gmail.com>
2019-01-13 01:53:25 +0800

    Update teacher_question_two_out.py

--- teacher_question_two_out.py
+++ teacher_question_two_out.py
@@ -1,26 +1,40 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jan  8 17:09:01 2019
+Created on Sun Jan 13 01:40:38 2019
 
-@author: Lenovo
+@author: liting
 """
+
 x = 0
 y = 0
 
 def GetInputNo():
-    global x
+    global x # why you need a global variable? 
     global y
     x = int(input('Please input first number:'))
     y = int(input('Please input second number:'))
 
 def SumTheVariable(x, y):
-    return int(int(x) + int(y))
+    return int(int(x) + int(y)) # int(x) + int(y) should be datatype integer, why you need to cast to int?
 
 def OutputResult(x, y, z):
     print('\nThe sum of ' + str(x) + ' and ' + str(y) + ' is ' + str(z) + '.')
-
+    # you can also do the printing as the code below
 GetInputNo()
 OutputResult(x, y, SumTheVariable(x, y))
 
-git remote add origin https://github.com/HansonSin/Teacher-Question-Python-3.git
-git push -u origin master
+
+##### OR You may do like this
+def GetInputNo():
+    x = int(input('Please input first number:'))
+    y = int(input('Please input second number:'))
+    return x, y
+
+def SumTheVariable(x, y):
+    return int(x) + int(y)
+
+def OutputResult(x, y, z):
+    print('\nThe sum of %d and %d is %d.' %( x, y, z))
+
+x, y = GetInputNo()
+OutputResult(x, y, SumTheVariable(x, y))
